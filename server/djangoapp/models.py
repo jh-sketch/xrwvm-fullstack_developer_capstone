feb70cf6fd5611326b7651cc15c6810248b3c8a0
jh-sketch <jhl321@yahoo.com>
2025-07-24 20:46:12 -0400

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,11 +1,6 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-
-# Create your models here.
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
@@ -15,10 +10,9 @@ class CarMake(models.Model):
         return self.name
 
 
-# Car Model model
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One
-    dealer_id = models.IntegerField()  # Refers to dealer in Cloudant DB
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField()
     name = models.CharField(max_length=100)
 
     CAR_TYPES = [
@@ -30,29 +24,19 @@ class CarModel(models.Model):
         ('HATCHBACK', 'Hatchback'),
     ]
 
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    type = models.CharField(
+        max_length=10,
+        choices=CAR_TYPES,
+        default='SUV'
+    )
+
     year = models.IntegerField(
-        validators=[MinValueValidator(2015), MaxValueValidator(2023)],
+        validators=[
+            MinValueValidator(2015),
+            MaxValueValidator(2023)
+        ],
         default=2023
     )
 
     def __str__(self):
         return f"{self.car_make.name} {self.name} ({self.year})"
-
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
-
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
