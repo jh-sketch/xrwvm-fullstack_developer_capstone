26136ab4b8eea697a10c004c2447acf4f8d9b906
jh-sketch <jhl321@yahoo.com>
2025-07-24 21:59:11 -0400

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,33 +10,33 @@ urlpatterns = [
     path('logout', views.logout_user, name='logout'),
     path('register', views.registration, name='registration'),
     path(
-        route='get_cars',
-        view=views.get_cars,
+        'get_cars',
+        views.get_cars,
         name='getcars'
     ),
     path(
-        route='get_dealers/',
-        view=views.get_dealerships,
+        'get_dealers/',
+        views.get_dealerships,
         name='get_dealers'
     ),
     path(
-        route='get_dealers/<str:state>',
-        view=views.get_dealerships,
+        'get_dealers/<str:state>',
+        views.get_dealerships,
         name='get_dealers_by_state'
     ),
     path(
-        route='dealer/<int:dealer_id>',
-        view=views.get_dealer_details,
+        'dealer/<int:dealer_id>',
+        views.get_dealer_details,
         name='dealer_details'
     ),
     path(
-        route='reviews/dealer/<int:dealer_id>',
-        view=views.get_dealer_reviews,
+        'reviews/dealer/<int:dealer_id>',
+        views.get_dealer_reviews,
         name='dealer_reviews'
     ),
     path(
-        route='add_review',
-        view=views.add_review,
+        'add_review',
+        views.add_review,
         name='add_review'
     ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
