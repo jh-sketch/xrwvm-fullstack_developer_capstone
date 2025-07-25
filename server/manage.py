bad2d0dd5c197a9a07669904c711b43fede7e29c
jh-sketch <jhl321@yahoo.com>
2025-07-24 22:07:32 -0400

    Update manage.py

--- server/manage.py
+++ server/manage.py
@@ -2,7 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+
 # This is a test change to trigger CI
+
+
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoproj.settings')
