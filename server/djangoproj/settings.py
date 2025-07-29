ff73e2086edfa2f1efa71972ccf2b78cc1bd3c60
jh-sketch <jhl321@yahoo.com>
2025-07-28 22:53:10 -0400

    Initial commit

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -20,10 +20,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'jhd001qs0545-8000.theianext-0-labs-prod-misc-tools-us-east-0.'
-    'proxy.cognitiveclass.ai',
-    'jhd001qs0545-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.'
-    'proxy.cognitiveclass.ai',
+    'jhd001qs0545-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'jhd001qs0545-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    'jhd001qs0545-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
